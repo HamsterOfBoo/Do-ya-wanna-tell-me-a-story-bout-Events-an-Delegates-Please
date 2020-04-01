9b72858d7625948f135733e28b9df24050f8b702
HamsterOfBoo <hamsterofboo@gmail.com>
2020-04-01 13:36:09 +0300

    Initial commit without some code

---
+++ Do ya wanna tell me a story bout Events an Delegates, Please/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Do_ya_wanna_tell_me_a_story_bout_Events_an_Delegates__Please
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
